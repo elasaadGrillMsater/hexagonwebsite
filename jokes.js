5d3822804f71ce0c6cdca06947caf90441b1d644
mhammed <mohamed.assaad.10@gmail.com>
2020-10-11 22:59:59 +0100

    hexagon-website

---
+++ jokes.js
@@ -0,0 +1,6 @@
+
+function joking() {
+    let jokes = ['','My great uncle died from drinking furniture polish. it was a slow death, but a beautiful finish.','Whats heavier, a tonne of water or a tonne of butane . Water, because butane is a lighter fluid','abdelhamid life','I threw an engagement ring at my girlfriend, but she dodged out of the way. It was a near Mrs.','Why shouldnt you kiss anyone on January 1st? Because its only the first date','It took America about 3 months to get covid , China got it right off the bat','what did the blind man fall into that well? cuz h couldnt seee that well','i wasnt hoping this joke would display','your mama is so fat,when she wears high heels she strikes oil','Agrima the all father','what do u call a white man with a big dick? micheal jackson','what is the worst part about eating bald pussy? putting the diapers back on(just a joke)'];
+    var i=Math.floor(Math.random()*jokes.length);
+    alert(jokes [i])
+}
